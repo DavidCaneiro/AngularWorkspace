e89179331f312f4cca918844df67d1571a7db5df
davidcaneiro <davidcaneiro@outlook.com>
2019-02-20 10:23:55 +0100

    back

--- catalogoCoches/cochesBackend/index.js
+++ catalogoCoches/cochesBackend/index.js
@@ -11,11 +11,27 @@ let express = require('express')
 let bodyParser = require('body-parser')
 let mongoose = require('mongoose')
 let cocheController = require('./cocheController')
+
+
+
 let puerto = 7777
 
 let app = express()
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+// midle ware
+app.use( (req, res, next) => {
+	//permitimos que las peticiones se puedan hacer desde cualquier sitio
+	res.header('Access-Control-Allow-Origin', '*')
+	//res.header('Access-Control-Allow-Origin', '192.168.0.11')
+	// configuramos las cabeceras que pueden llegar
+	res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method' )
+	// configuramos los métodos que nos pueden llegar
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+	next(); // para que se salga de esta función
+})
+
 
 
 // res => respuesta | req => lo que viene como por ejemplo parametros
